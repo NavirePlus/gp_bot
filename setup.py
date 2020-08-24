2226807eb3874c48ea7edb39b08ce5b8395fa181
cielo <cielo.fox@gmail.com>
2020-08-25 00:02:51 +0900

    setup.pyを追加

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gp_bot',
+    version='1.1.0',
+    url='https://github.com/NavirePlus/gp_bot',
+    license='MIT',
+    author='cielo',
+    author_email='cielo.fox@gmail.com',
+    description='Gomi-People bot for Twitter',
+    install_requires=['setuptools'],
+    packages=["gp_bot"]
+)
